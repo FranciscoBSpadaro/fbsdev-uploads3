7701dc419b8733653c018c92d07f1f73a105223a
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-06-24 18:52:55 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+// multer importado para tratar errorHandler de arquivo grande 
+const multer = require('multer')
+
+
+/**
+ * Database setup
+ */
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }
+)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+)
+
+app.use(require('./routes'))
+
+
+app.listen(3000)
+// error handlers
+// codigo de erro no upload caso arquivo for muito grande
+app.use(function (err, req, res, next) {
+  if (err instanceof multer.MulterError) {
+  res.status(418).send('File is Too Large!')
+}})
---
+++ src/routes.js
@@ -0,0 +1,43 @@
+const routes = require('express').Router()
+const multer = require('multer')
+const multerConfig = require('./config/multer')
+
+const Post = require('./models/Post')
+
+routes.get('/posts', async (req, res) => {
+  const posts = await Post.find()
+
+  return res.json(posts)
+})
+// adicionado try catch para evitar error para request vazio
+routes.post('/posts', multer(multerConfig).single('file'), async (req, res) => {
+  try{
+  const { originalname: name, size, key, location: url = '' } = req.file
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url
+  })
+
+  return res.json(post)
+}
+catch (error) {
+  return res.status(400).send('Invalid Request')
+}
+})
+// adicionado try e catch caso nao achar o id do objeto para deletar para nao derrubar o servidor
+routes.delete('/posts/:id', async (req, res) => {
+  try{
+  const post = await Post.findById(req.params.id)
+  await post.remove()
+  return res.status(200).send('Image Deleted')
+  }
+  catch (error) {
+  return res.status(400).send('ID Not Found or invalid ')
+}
+});
+
+
+module.exports = routes

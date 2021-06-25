551644942b0665ceb401a1f19a7510a618651730
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-06-25 00:26:11 -0300

    allow XMLHttpRequest

--- index.js
+++ index.js
@@ -25,9 +25,8 @@ mongoose.connect(
 
 app.use(cors())
 app.use((req, res, next) =>{
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type,Accept, Authortization')
-  res.setHeader('Acces-Control-Allow-Methods','GET, POST, PATCH, DELETE')})
+  res.setHeader('Access-Control-Allow-Origin', '*')})
+  
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))

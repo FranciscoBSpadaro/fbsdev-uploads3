38fdf659b370436eb0212910b632dba493655d7f
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-06-26 15:29:59 -0300

    allowCrossDomain

--- allowcrossdomain.js
+++ allowcrossdomain.js
@@ -1,5 +1,5 @@
 const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://fbsdev-uploads3.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', 'https://fbsdev-uploadss3.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 

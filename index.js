87415f4976b229abdf0b3d52c147743210c11096
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-01 20:26:50 -0300

    rollback mongodb params

--- index.js
+++ index.js
@@ -16,23 +16,18 @@ const multer = require('multer')
  * Database setup
  */
 
+ try {
+  mongoose.connect(
+   process.env.MONGO_URL,
+   {
+     useUnifiedTopology: true,
+     useNewUrlParser: true
+    }
+    )
+    } catch (error) {
+      handleError(error)
+    }
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  autoIndex: false, // Don't build indexes
-  poolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4 // Use IPv4, skip trying IPv6
-};
-mongoose.connect(process.env.MONGO_URL,options, function(error) {
-  if (error){
-    res.status(503).json('error mongo db atlas')
-  }
-});
 
 app.use(cors())
 

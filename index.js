e959adc581291b22f3276ddb9165a9f2e5667520
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-06-30 21:57:30 -0300

    new mongodb params

--- index.js
+++ index.js
@@ -15,17 +15,24 @@ const multer = require('multer')
 /**
  * Database setup
  */
- try {
- mongoose.connect(
-  process.env.MONGO_URL,
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
+
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoIndex: false, // Don't build indexes
+  poolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+};
+mongoose.connect(process.env.MONGO_URL,options, function(error) {
+  if (error){
+    res.status(503).json('error mongo db atlas')
   }
-)
-} catch (error) {
-  handleError(error);
-}
+});
 
 app.use(cors())
 

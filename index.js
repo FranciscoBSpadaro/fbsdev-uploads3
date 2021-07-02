8feacbf4bb67961c109be8caff100228232914e4
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-01 21:35:48 -0300

    rollback cors and remove app use files folders

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const path = require('path')
-
+const cors = require('cors')
 const app = express()
 
 // multer importado para tratar errorHandler de arquivo grande 
@@ -29,14 +29,22 @@ const multer = require('multer')
     }
 
 
+app.use(cors())
+
+// liberar request to XMLHttpRequest da origin do meu front end
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+})
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
-app.use(
+/*app.use(
   '/files',
   express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
-)
+)//** */
 
 app.use(require('./routes'))
 

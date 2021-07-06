b231e6a2daab02212b196166bc239cd68bfee2eb
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-06 19:56:48 -0300

    cors no topo

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   )
   if (req.method === "OPTIONS") {
     res.header(

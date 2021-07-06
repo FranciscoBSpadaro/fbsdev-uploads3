4d0a8885f8fb110f1e7bdabe7c15e74cf8f6cc99
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-06 20:29:18 -0300

    cors no topo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const multer = require('multer')
 
 //liberar request to XMLHttpRequest da origin do meu front end
 app.use('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://fbsdev-uploadss3.herokuapp.com/')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
   res.header('Access-Control-Allow-Headers',  'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   if ('OPTIONS' == req.method) {

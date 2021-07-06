08914e8281c5d1ff241504747ec4f56c309a007f
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-06 20:00:13 -0300

    cors no topo

--- index.js
+++ index.js
@@ -12,16 +12,13 @@ const multer = require('multer')
 
  //liberar request to XMLHttpRequest da origin do meu front end
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   )
   if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods","POST, PUT, PATCH, GET, DELETE")
     return res.status(200).json({})
   }
   app.use(cors())

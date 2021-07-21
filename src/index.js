3923186163b847bf7e6ccf642791fbdac097670a
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-21 20:33:22 -0300

    use all cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const path = require('path')
-// const cors = require('cors')
+const cors = require('cors')
 const app = express()
 
 // multer importado para tratar errorHandler de arquivo grande 
@@ -44,6 +44,18 @@ app.use('*', function(req, res, next) {
 
 
 // app.use(cors())
+app.use('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://fbsdev-uploadss3.herokuapp.com')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers',  'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if ('OPTIONS' == req.method) {
+  res.sendStatus(200)
+  } else {
+    app.use(cors())
+    next()
+  }
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))

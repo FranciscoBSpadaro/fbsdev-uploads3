9d34d90d2eb253db6d6374b8062fe431db2ccc7d
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-22 16:28:30 -0300

    change post save

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
-const aws = require('aws-sdk')
-const fs = require('fs')
-const path = require('path')
-const { promisify } = require('util')
+const mongoose = require("mongoose")
+const aws = require("aws-sdk")
+const fs = require("fs")
+const path = require("path")
+const { promisify } = require("util")
 
-const s3 = new aws.S3()
+const s3 = new aws.S3();
 
 const PostSchema = new mongoose.Schema({
   name: String,
@@ -13,41 +13,34 @@ const PostSchema = new mongoose.Schema({
   url: String,
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+  },
+})
+PostSchema.pre("save", function () {
+  if (!this.url) {
+    this.url = `${process.env.APP_URL}/files/${this.key}`
+  } else process.env.STORAGE_TYPE === "s3"
+  {
+    return s3.putObject({
+        Bucket: process.env.BUCKET,
+        Key: this.key,
+      })
+      .promise()
   }
 })
 
-PostSchema.pre('save', async function () {
-  (process.env.STORAGE_TYPE === 's3') 
-    try {
-      const response = await s3.putObject({
-          Bucket: process.env.BUCKET_NAME,
-          Key: this.key
-        })
-        .promise()
-      console.log(response.status)
-    } catch (response_1) {
-      console.error(response_1.error)
-    }
-})
-
-PostSchema.pre('remove', async function () {
-  if (process.env.STORAGE_TYPE === 's3') {
-    try {
-      const response = await s3.deleteObject({
-          Bucket: process.env.BUCKET_NAME,
-          Key: this.key
-        })
-        .promise()
-      console.log(response.status)
-    } catch (response_1) {
-      console.error(response_1.error)
-    }
+PostSchema.pre("remove", function () {
+  if (process.env.STORAGE_TYPE === "s3") {
+    return s3.deleteObject({
+        Bucket: process.env.BUCKET,
+        Key: this.key,
+      })
+      .promise();
   } else {
     return promisify(fs.unlink)(
-      path.resolve(__dirname, '..', '..', 'tmp', 'uploads', this.key)
+      path.resolve(__dirname, "..", "..", "temp", "uploads", this.key)
     )
   }
 })
 
-module.exports = mongoose.model('Post', PostSchema)
+module.exports = mongoose.model("Post", PostSchema)

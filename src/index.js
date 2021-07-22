9c392be2c2cec4caf71d9cc109f910e50e848294
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-21 21:41:53 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -43,18 +43,7 @@ app.use('*', function(req, res, next) {
     }
 
 
-// app.use(cors())
-app.use('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://fbsdev-uploadss3.herokuapp.com')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  res.header('Access-Control-Allow-Headers',  'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  if ('OPTIONS' == req.method) {
-  res.sendStatus(200)
-  } else {
-    app.use(cors())
-    next()
-  }
-})
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))

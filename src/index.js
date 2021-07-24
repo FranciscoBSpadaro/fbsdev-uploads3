7bf8e1ba5c0601c3928bf6306b92b628b982f13a
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-24 20:21:39 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -32,10 +32,10 @@ app.use((req, res, next) => {
   app.use(cors())
   next()
 })  */
-app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use(cors())
 
 app.use('/files',express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
 

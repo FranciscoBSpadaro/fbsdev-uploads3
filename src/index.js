972243bbb2ae6ec08d4963c3826cca362109872e
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-24 19:17:00 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -12,18 +12,8 @@ const app = express()
 const multer = require('multer')
 
 
-/**liberar request to XMLHttpRequest da origin do meu front end
-app.use('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://fbsdev-uploadss3.herokuapp.com')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-  res.header('Access-Control-Allow-Headers',  'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  if ('OPTIONS' == req.method) {
-  res.sendStatus(200)
-  } else {
-    app.use(cors())
-    next()
-  }
-})
+
+
 /**
  * Database setup
  */
@@ -41,9 +31,16 @@ app.use('*', function(req, res, next) {
     }
 
 
+// liberar request to XMLHttpRequest da origin do meu front end
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://fbs-dev-uploads3.herokuapp.com/")
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS')
+  app.use(cors())
+  next()
+})
+ 
 
-
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
